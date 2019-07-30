cda596584645803bd8d4d6905949c55c800fbaa7
unknown <nikoladimitrijevic1991@gmail.com>
2019-07-30 18:46:36 +0200

    john smith

---
+++ js/common.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+
+    // $('.menu-burger').on('click',function(){
+    //     var menu = $(".menu"),
+    //         subMenu = $('.menu-content');
+    //     if(!menu.hasClass('__active')){
+    //         $('.main-username').addClass('hide-user');
+    //         menu.addClass('__active',100).promise().done(function(){
+                    
+    //             subMenu.delay(800).queue(function() {
+    //             $(this).addClass('__active');
+    //                 $(this).dequeue();
+    //             });
+
+    //             $('.menu-burger').addClass('__active');
+
+    //         });
+    //     }
+    //     else{
+    //         $(".menu,.menu-content,.menu-burger").removeClass('__active',100)
+    //         $('.main-username').removeClass('hide-user');       
+    //     }
+        
+    // });
+
+    $(window).on('scroll',function(){
+        if($('.instagram-heading > h1').hasClass('go')){
+            $('.instagram-section').addClass('black-bg')
+        }else{
+            $('.instagram-section').removeClass('black-bg')
+        }
+    });
+
+    $('.top-bottom-link').on("click",function() {
+        $('html, body').animate({ scrollTop: 0 }, 'slow');
+    });
+
+    $(window).on('scroll',function() {    
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 100) {
+            $(".top-bottom-link").addClass("show-link");
+        } else {
+            $(".top-bottom-link").removeClass("show-link");
+        }
+    });
+
+   $("body").niceScroll({
+        cursorwidth:12,
+        cursoropacitymin:0.4,
+        cursorcolor:'#d4bf4b',
+        cursorborder:'none',
+        cursorborderradius:4,
+        autohidemode:'leave'
+    });
+    $('.nicescroll-rails').css('z-index','999')
+
+})
